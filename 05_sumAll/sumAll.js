d5c9706aefcda6421201e0f0f3655c31cfeac975
Gautam Gupta <ggupta78@gmail.com>
2023-02-09 19:55:07 +0530

    complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
 const sumAll = function() {
+  let start = arguments[0];
+  let end = arguments[1];
 
+  if (start < 0 || end < 0) {
+    return 'ERROR';
+  }
+
+  if (typeof start != 'number' || typeof end != 'number') {
+    return 'ERROR';
+  }
+
+  if (start > end) {
+    start = end;
+    end = arguments[0];
+  }
+
+  let count = end - start + 1;
+
+  return count * ((start + end) / 2);
 };
 
 // Do not edit below this line
