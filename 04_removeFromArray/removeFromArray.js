6d125e4ff8356f2da2ff58bf3afddd16a963f581
Gautam Gupta <ggupta78@gmail.com>
2023-02-09 17:24:42 +0530

    complete remove from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
+  let finalArray = arguments[0];
 
+  for (let index = 1; index < arguments.length; index++) {
+    const foundIndex = finalArray.indexOf(arguments[index]);
+    if (foundIndex > -1)
+      finalArray.splice(foundIndex, 1);
+  }
+
+  return finalArray;
 };
 
 // Do not edit below this line
