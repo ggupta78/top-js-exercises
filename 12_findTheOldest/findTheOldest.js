ff7fd8c5dbadfc20c820f1a62be0847abcb0ec87
Gautam Gupta <ggupta78@gmail.com>
2023-02-17 15:55:30 +0530

    complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldest = { age: 0 };
 
+  people.forEach(person => {
+    let currentAge = 0;
+    if (Object.hasOwn(person, "yearOfDeath")) {
+      currentAge = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      currentAge = (new Date()).getFullYear() - person.yearOfBirth;
+    }
+
+    if (currentAge > oldest.age) {
+      oldest = person;
+      oldest.age = currentAge;
+    }
+  });
+
+  return oldest;
 };
 
 // Do not edit below this line
