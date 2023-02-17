db3201580361ceb64a1d80be36a02a4b8bc82bde
Gautam Gupta <ggupta78@gmail.com>
2023-02-17 13:43:26 +0530

    complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+  return list.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  return list.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(n, p) {
+  return Math.pow(n, p);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+
+  return factorial(n - 1) * n;
 };
 
 // Do not edit below this line
