28b5b6ffd78a4efce02382d0e0178c0e2f5b7a32
Gautam Gupta <ggupta78@gmail.com>
2023-02-17 14:09:44 +0530

    complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  const terms = [1, 1];
 
+  if (n.isNan) {
+    n = Number.parseInt(n);
+    if (n.isNan) return "OOPS";
+  }
+
+  if (n < 0) return "OOPS";
+
+  if (n < 3) return 1;
+
+  for (let index = 3; index <= n; index++) {
+    terms.push(terms[index - 3] + terms[index - 2]);
+  }
+
+  return terms[n - 1];
 };
 
 // Do not edit below this line
