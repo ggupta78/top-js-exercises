c9f1946ac0d33e8e4d5d61a63b426933eee156b6
Gautam Gupta <ggupta78@gmail.com>
2023-02-17 13:58:50 +0530

    complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const stripNonAlphabets = function(phrase) {
+  let cleanPhrase = "";
+  for (let index = 0; index < phrase.length; index++) {
+    if (phrase[index].match(/[a-z]/i)) cleanPhrase += phrase[index];
+  }
+  return cleanPhrase;
+};
 
+const palindromes = function (phrase) {
+  phrase = stripNonAlphabets(phrase.toLowerCase());
+  const reversedPhrase = phrase.split('').reverse().join('');
+
+  return (phrase === reversedPhrase) ? true : false;
 };
 
 // Do not edit below this line
